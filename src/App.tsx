bbe6035ccb40aa75c98d365885b8622f7dc63e20
LLColeJ.ETH <cole511@protonmail.com>
2024-02-17 02:03:35 -0500

    setup react

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <label>
+          Text input: <input name="myInput" />
+        </label>
+      </div>
+    </>
+  );
+}
+
+export default App;
